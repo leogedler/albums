a5f9d1c951bd8bb284049df412b8687bd0672d9d
Leonardo Gedler <leonardo.gedler@macys.com>
2018-10-23 00:55:30 -0300

    Finished basic logic and styling

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbuList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      albums: []
+    };
+  }
+
 
   async componentWillMount() {
     try {
       const response = await axios.get('https://rallycoding.herokuapp.com/api/music_albums');
-      console.log('Response', response.data);
+      this.setState({ albums: response.data });
     } catch (error) {
       console.log('Error', error);
     }
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+  }
+
   render() {
     return (
-      <View>
-        <Text>Album List!!</Text>
-      </View>
+      <ScrollView>
+       {this.renderAlbums()}
+      </ScrollView>
     );
   }
 }
